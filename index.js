447247126e7e612146da2bbf439a70d7c28b9dfc
tonyhurtado <aahurtado@gmail.com>
2025-08-22 17:29:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
+import axios from "axios";
+
 const app = express();
 
-// existing routes
+// --- routes ---
 app.get("/auth/fb/callback",(req,res)=>res.send("Callback OK ✔"));
-app.get("/webhook",(req,res)=>res.send(req.query["hub.challenge"]||"OK"));
-app.post("/webhook",express.json(),(req,res)=>{console.log(req.body);res.sendStatus(200);});
 
-// new login route
+app.get("/webhook",(req,res)=>res.send(req.query["hub.challenge"]||"OK"));
+
+app.post("/webhook",express.json(),(req,res)=>{
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 app.get("/auth/fb/login",(req,res)=>{
   const u=new URL("https://www.facebook.com/v21.0/dialog/oauth");
   u.searchParams.set("client_id",process.env.FB_APP_ID);
@@ -16,6 +22,21 @@ app.get("/auth/fb/login",(req,res)=>{
   res.redirect(u.toString());
 });
 
-// listen
+app.get("/auth/fb/token", async (req,res)=>{
+  const {code}=req.query;
+  const r1=await axios.get("https://graph.facebook.com/v21.0/oauth/access_token",{params:{
+    client_id:process.env.FB_APP_ID,
+    client_secret:process.env.FB_APP_SECRET,
+    redirect_uri:process.env.FB_REDIRECT,
+    code}});
+  const r2=await axios.get("https://graph.facebook.com/v21.0/oauth/access_token",{params:{
+    grant_type:"fb_exchange_token",
+    client_id:process.env.FB_APP_ID,
+    client_secret:process.env.FB_APP_SECRET,
+    fb_exchange_token:r1.data.access_token}});
+  res.json(r2.data);
+});
+
+// --- start server ---
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,"0.0.0.0",()=>console.log("Listening on "+PORT));
