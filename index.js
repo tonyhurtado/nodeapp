cf0f9c1ba00990b04332ac05c6c8be974231e645
tonyhurtado <aahurtado@gmail.com>
2025-08-25 17:13:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, _res, next) => {
 });
 
 /* ---- sanity check ---- */
-app.get("/", (_req,res)=>res.send("OK v7"));
+app.get("/", (_req,res)=>res.send("OK v8"));
 
 /* ---- webhook verify/receive ---- */
 app.get("/webhook", (req, res) => {
